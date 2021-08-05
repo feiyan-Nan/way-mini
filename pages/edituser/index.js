baacb7217efa1a675f48da08e1453e44479ddb89
kenny <879464294@qq.com>
2021-08-05 16:52:25 +0800

    增加个信息编辑页面

---
+++ pages/edituser/index.js
@@ -0,0 +1,17 @@
+// pages/editUser/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    tmp: 'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+})
